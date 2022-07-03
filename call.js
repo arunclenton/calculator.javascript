55f31126367a6fb064aa75382abdb08c2fc3b3ee
arunclenton <arunclenten@gmail.com>
2022-07-03 21:24:49 +0530

    add

---
+++ call.js
@@ -0,0 +1,49 @@
+
+var output = document.getElementById('output-screen');
+
+function display(num){
+
+output.value += num;
+
+console.log(num);
+
+}
+// function multi(){
+//     var a = document.getElementById('multi').innerText;
+    
+//      output.value += a;
+// }
+
+// function add(){
+//     var a = document.getElementById('add').innerText;
+    
+//      output.value += a;
+// }
+
+// function sub(){
+//     var a = document.getElementById('sub').innerText;
+    
+//      output.value += a;
+// // }
+// function calculate(){
+//     var a = document.getElementById('calculate').innerText;
+    
+//     output.value += a;
+// }
+function clr(num){
+    var x = document.getElementById("output-screen").value='';
+    x.value = num;
+    console.log(x);
+}
+function del(num){
+    var x = document.getElementById("output-screen").value='';
+    x.value = num;
+    console.log(x);
+}
+function calculat(){
+    var x = document.getElementById("output-screen");
+
+}
+
+
+
